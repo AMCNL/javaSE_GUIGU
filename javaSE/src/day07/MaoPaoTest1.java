e10f0cd0c8c7903f7660019e514283b5fa5294e4
lichunnan <li.chunnan@xyb2b.com>
2021-03-18 13:58:52 +0800

    111

--- javaSE/src/day07/MaoPaoTest1.java
+++ javaSE/src/day07/MaoPaoTest1.java
@@ -6,6 +6,22 @@ package day07;
  */ 
 public class MaoPaoTest1 {
     public static void main(String[] args) {
+        int[] arr = new int[]{12,5,89,45,3,67,88,55};
+
+        int tmp ;
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = 0; j <arr.length-i-1 ; j++) {
+                if(arr[j]>arr[j+1]){
+                    tmp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+
+        for (int i : arr) {
+            System.out.print(i + "\t");
+        }
 
     }
 }
